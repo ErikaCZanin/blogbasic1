888fa6b9c15fd8f54f665429d923251bcc9f07a1
ErikaCZanin <erikaczbu@gmail.com>
2024-06-22 22:09:49 -0300

    Atualizando logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import livro2 from './assets/livro2.png'
 import './App.css'
 
 function App() {
