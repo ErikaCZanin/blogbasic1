ebe99e1d09d357ec5ec9e2a3022c5e5fbac6eac4
ErikaCZanin <erikaczbu@gmail.com>
2024-06-22 21:40:21 -0300

    Atualizando conteúdo e estilo

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,21 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <body>
+    <div className='container'>
+        <h1>Teste</h1>
+        <h2> Titulo 1</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
+        <h1>Teste</h1>
+        <h2> Titulo 2</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
+        <h1>Teste</h1>
+        <h2> Titulo 3</h2>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
+        </div>
+        </body>
   )
 }
 
