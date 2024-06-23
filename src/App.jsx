59ea7a2e40f6920d473e82c58849e491506d6823
ErikaCZanin <erikaczbu@gmail.com>
2024-06-23 19:43:40 -0300

    Atualizando css e texto

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,12 @@ function App() {
   return (
   <body>
     <div className='container'>
-        <h1>Teste</h1>
-        <h2> Titulo 1</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
-        <h1>Teste</h1>
-        <h2> Titulo 2</h2>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
-        <h1>Teste</h1>
-        <h2> Titulo 3</h2>
+        <h1>Astros</h1>
+        <h2>Sol</h2>
+        <p>O Sol é a estrela do sistema solar. Todos os corpos celestes desse sistema, inclusive a Terra, giram em torno dele. O diâmetro do Sol é de 1,392 milhão de quilômetros. Ele é composto principalmente por hidrogênio e hélio, além de ferro, níquel, oxigênio, silício, carbono, nitrogênio, enxofre, etc.</p>
+        <h2>Terra</h2>
+        <p>O planeta Terra é um dos quatro planetas telúricos (de formação rochosa) do Sistema Solar, sendo os outro: Mercúrio, Vênus e Marte. É o único planeta do Sistema Solar em que existe água em estado líquido, característica que junto ao oxigênio e a temperatura média de 14 °C possibilitam a vida no planeta.</p>
+        <h2>Marte</h2>
         <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis iusto placeat velit unde inventore ea non dolorum ducimus amet suscipit dicta iure iste, ullam recusandae est eveniet tenetur laudantium assumenda!</p>
         </div>
         </body>
